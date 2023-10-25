6d39406a089f2419051ddc0954b69c5310e374cb
陈乐章 <melodychen@finn-chen.milton.edu>
2023-10-25 09:02:00 -0400

    made default pony and made an object of pony

---
+++ src/Ponies.java
@@ -0,0 +1,14 @@
+public class Ponies {
+    // true/false = boolean
+    // numbers = int, double
+    // words = String
+
+    boolean isUnicorn = false;
+    boolean isPegasus = false;
+
+    double size = 20.56;
+    String name = "lily";
+    String color = "pink";
+
+
+}
