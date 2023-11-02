05a2ea84beaedba3d72e4c425a858d6d061dbe34
finnchen26 <mcisns7@gmail.com>
2023-11-02 09:02:35 -0400

    made new constructor

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Ponies p;
-        p = new Ponies();
-        System.out.println(p.isUnicorn);
-        p.isUnicorn = true;
-        System.out.println(p.isUnicorn);
-        System.out.println(p.name);
+        p = new Ponies("bobby", "pink", 20.56, true, false);
+        System.out.println("pony p's name is " + p.name);
         p.describePony();
 
         Ponies a;
-        a = new Ponies();
-        a.name = "teddy";
+        a = new Ponies("teddy", "pink", 20.56, false, false);
         System.out.println("pony a's name is " + a.name);
+        a.describePony();
+
+        Ponies q;
+        q = new Ponies("qal", "green", 21.34, false, true);
+        System.out.println("pony q's name is " + q.name);
+        q.describePony();
 
     }
 }
--- src/Ponies.java
+++ src/Ponies.java
@@ -3,14 +3,23 @@ public class Ponies {
     // numbers = int, double
     // words = String
 
-    public boolean isUnicorn = false;
-    public boolean isPegasus = false;
+    public boolean isUnicorn;
+    public boolean isPegasus;
 
-    public double size = 20.56;
-    public String name = "lily";
-    public String color = "pink";
+    public double size;
+    public String name;
+    public String color;
 
-    public void describePony(){
+    public Ponies(String pName, String pColor, double pSize, boolean pIsUnicorn, boolean pIsPegasus) { //constructor, no return type, has to have same name as class
+        System.out.println("made a new pony!");
+        isUnicorn = pIsUnicorn;
+        isPegasus = pIsPegasus;
+        size = pSize;
+        name = pName;
+        color = pColor;
+    }
+
+    public void describePony(){ //void = return type
         System.out.println("this is pony " + name + " they are the color " + color + " with a size of " + size + ". It is " + isUnicorn + " that the pony is a Unicorn. It is " + isPegasus + " that the pony is a Pegasus.");
     }
 
